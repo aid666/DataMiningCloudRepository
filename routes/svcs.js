da31552fafbac1456a7ed83cefcf81b1e50a44af
aid666 <aid666@gmail.com>
2015-07-09 00:37:03 +0800

    Update svcs mock

--- routes/svcs.js
+++ routes/svcs.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var services = [
+var svcs = [
     {
       key: 1,
       name: "name 1"
@@ -14,7 +14,26 @@ var services = [
 
 /* GET svcs listing. */
 router.get('', function(req, res, next) {
-  res.json(services);
+  res.json(svcs);
+});
+
+/* POST svcs listing. */
+router.post('', function(req, res, next) {
+  var new_svc = JSON.parse(res.body);
+  svcs.push(new_svc)
+  res.json(new_svc);
+});
+
+/* GET svc details */
+router.get('/:key', function(req, res, next){
+  for (var i = 0; i < svcs.length; i++) {
+    var svc = svcs[i];
+    if(svc.key == res.params.key){
+      res.json(svc);
+      break;
+    }
+
+  }
 });
 
 module.exports = router;
